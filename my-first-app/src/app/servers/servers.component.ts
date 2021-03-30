37975ccf7aed3af6fd67dbd2bcbc916e0f595196
GirishSrinivas <girish.s.srinivas@gmail.com>
2021-03-29 23:24:04 -0500

    Components usage

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  /*
+    Regular selector
+    selector: 'app-servers'
+  */
+
+  /*
+   selector as attribute
+   selector: '[app-servers]',
+  */
+
+  // selector as class attribute
+  selector: '.app-servers',
   template: `<app-server></app-server>
             <app-server></app-server>`,
   styleUrls: ['./servers.component.css']

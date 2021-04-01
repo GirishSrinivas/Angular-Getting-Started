82f0b29133fcb23ede0a53f55cd90d9fdf26432b
GirishSrinivas <girish.s.srinivas@gmail.com>
2021-04-01 03:47:55 -0500

    Property binding example

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -13,13 +13,16 @@ import { Component, OnInit } from '@angular/core';
 
   // selector as class attribute
   selector: '.app-servers',
-  template: `<app-server></app-server>
-            <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer: boolean = false;
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
   }
